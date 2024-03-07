de96efa0082ca1cad78ad49eb03003f807665886
Radojica Popovic <radojicapopovic@icloud.com>
2024-03-07 15:05:13 +0100

    - Formatting changes, Changes to the Prettier setup

--- CHANGLELOG.md
+++ CHANGLELOG.md
@@ -5,7 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.2] - 07-03-2024
+
+### Added
+
+- Formatting changes, Changes to the Prettier setup
 
 ## [0.0.1] - 07-03-2024
+
 ### Added
+
 - Intital Commit, Basic setup for BrainWave | React, Vite & TailwindCSS Setup
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 export default function App() {
   return (
@@ -19,9 +18,7 @@ export default function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
